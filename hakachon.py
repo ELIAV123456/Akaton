8de60b64fe508979f4deee664fd5173ef6f5872b
your-name <your-email>
2026-04-16 11:47:24 +0300

    mocec

---
+++ hakachon.py
@@ -0,0 +1,40 @@
+from fuzzywuzzy import fuzz
+
+def date_input(schedules):
+    week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+    print("welcome")
+    print("share with me what your next week schedules")
+
+    for day in range(7):
+        print(week[day])
+        while in schedules[day][]:
+
+def schedules_
+
+def information_unity():
+    pass
+def about_you(choose):
+    pass
+
+def free_timescale():
+    pass
+
+def history():
+
+    pass
+
+
+def relevant_units(choose):
+    pass
+
+def
+
+def about_you(choose):
+    pass
+
+def welcome():
+    pass
+
+def main():
+
